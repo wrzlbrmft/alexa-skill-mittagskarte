5db8653963a01cb7c6df36b054419f121579bfdc
matthias <matthias@calvin>
2017-02-12 05:27:51 +0100

    request and cheerio

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+import * as request from "request";
+import * as cheerio from "cheerio";
+
+request("http://www.alte-raffinerie.de/index.php/Essen.html", (error, response, body) => {
+	let $ = cheerio.load(body);
+	$("#lunch").find("strong").each((index, element) => {
+		console.log($(element).text());
+	});
+});
