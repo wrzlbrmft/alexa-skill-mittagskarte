8e806b799133d175bc231b92f94584342332dd7b
matthias <matthias@calvin>
2017-02-20 00:31:42 +0100

    example data

--- src/lambda/ParserExample.ts
+++ src/lambda/ParserExample.ts
@@ -1,7 +1,8 @@
 import { AbstractParser } from "./AbstractParser";
 import { MenuDay } from "./MenuDay";
-import { Weekday } from "./Weekday";
+import { Weekday, weekdays } from "./Weekday";
 import { MenuWeek } from "./MenuWeek";
+import { Dish } from "./Dish";
 
 export class ParserExample extends AbstractParser {
 	public constructor(html?: string) {
@@ -9,10 +10,22 @@ export class ParserExample extends AbstractParser {
 	}
 
 	public parseMenuDay(weekday: Weekday): MenuDay {
-		return null;
+		let menuDay: MenuDay = new MenuDay();
+
+		for (let i: number = 0; i < 3; i++) {
+			menuDay.addDish(new Dish(`Menü ${i} am ${weekdays.get(weekday)}`));
+		}
+
+		return menuDay;
 	}
 
 	public parseMenuWeek(): MenuWeek {
-		return null;
+		let menuWeek: MenuWeek = new MenuWeek();
+
+		for (let weekday: number = Weekday.Monday; weekday <= Weekday.Friday; weekday++) {
+			menuWeek.put(weekday, this.parseMenuDay(weekday));
+		}
+
+		return menuWeek;
 	}
 }
