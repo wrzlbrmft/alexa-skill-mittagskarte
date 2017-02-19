80ad36f9f150f9f16f4378cee4bfe265fb356ba7
matthias <matthias@calvin>
2017-02-20 00:22:20 +0100

    weekdays

--- src/lambda/Weekday.ts
+++ src/lambda/Weekday.ts
@@ -1,3 +1,5 @@
+import { NumberMap } from "./NumberMap";
+
 export enum Weekday {
 	Sunday = 0,
 	Monday,
@@ -7,3 +9,12 @@ export enum Weekday {
 	Friday,
 	Saturday
 }
+
+export let weekdays: NumberMap<string> = new NumberMap<string>();
+weekdays.put(Weekday.Sunday, "Sonntag");
+weekdays.put(Weekday.Monday, "Montag");
+weekdays.put(Weekday.Tuesday, "Dientag");
+weekdays.put(Weekday.Wednesday, "Mittwoch");
+weekdays.put(Weekday.Thursday, "Donnerstag");
+weekdays.put(Weekday.Friday, "Freitag");
+weekdays.put(Weekday.Saturday, "Samstag");
